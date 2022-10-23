fc950eca69a22a429338ae0527c170976b0c0f16
unknown <azametica1@etf.unsa.ba>
2022-10-23 21:07:50 +0200

    Initial commit

---
+++ src/main/java/ba/unsa/etf/rpr/Sat.java
@@ -0,0 +1,41 @@
+package ba.unsa.etf.rpr;
+
+public class Sat {
+    int sati,minute,sekunde;
+
+    public Sat(int sati, int minute, int sekunde) {
+        Postavi(sati,minute,sekunde);
+    }
+    public void Postavi(int h, int m, int s) {
+        sati=h;
+        minute=m;
+        sekunde=s;
+    }
+    public void Sljedeci() {
+        sekunde++;
+        if (sekunde==60) {
+            sekunde=0;
+            minute++;
+        }
+        if (minute==60) { minute=0; sati++; }
+        if (sati==24) sati=0;
+    }
+    public void Prethodni() {
+        sekunde--;
+        if (sekunde==-1) { sekunde=59; minute--; }
+        if (minute==-1) { minute=59; sati--; }
+        if (sati==-1) sati=23;
+    }
+
+    public void PomjeriZa(int pomak) {
+        if (pomak>0) for (int i=0; i<pomak; i++) Sljedeci();
+        else for (int i=0; i<=pomak; i++) Prethodni();
+    }
+
+    int DajSate() { return sati; }
+    int DajMinute() { return minute; }
+    int DajSekunde() { return sekunde; }
+    void Ispisi() {
+        System.out.println("Sati: "+ sati + ":"+minute+":"+sekunde);
+    }
+    };
